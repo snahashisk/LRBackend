900c7b7f58c2047e4255005d6b1ec30b455a5448
snahashis <snahashiskanrar@gmail.com>
2026-05-02 22:14:09 +0530

    feat: add health check and server time controllers for system monitoring

--- src/controllers/health.controller.js
+++ src/controllers/health.controller.js
@@ -1,20 +1,26 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import mongoose from "mongoose";
+import { redisConnection } from "../utils/redis.js";
 
 const healthCheck = asyncHandler(async (req, res) => {
   const health = {
     database: "Disconnected",
+    redis: "Disconnected",
     uptime: "",
   };
 
   try {
     const dbStatus = mongoose.connection.readyState;
     health.database = dbStatus === 1 ? "Connected" : "Connecting";
+    health.redis = redisConnection.status === "ready" ? "Connected" : "Connecting";
     health.uptime = process.uptime();
 
     if (dbStatus !== 1) {
       throw new ApiError(503, "Database is not ready.");
+    }
+    if (redisConnection.status !== "ready") {
+      throw new ApiError(503, "Redis is not ready.");
     }
 
     return res.status(200).json(new ApiResponse(200, health, "Server is healthy!!"));
